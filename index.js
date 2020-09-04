05b44d0deaa374c04cb3d849e6a31ab6db3f513e
sincere-trainer-5337 <eric.m.lombardo@gmail.com>
2020-09-04 16:57:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ function moveDodgerLeft() {
   // remove px / turn into number / assign dodger to left - 1
   var leftNumbers = dodger.style.left.replace('px', '');
   var left = parseInt(leftNumbers, 10);
-  if (dodger > 0) {
-
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
   }
-  dodger.style.left = `${left - 1}px`;
 }
 function moveDodgerRight() {
   // get dodger / remove px / turn into number / assign dodger to left - 1
