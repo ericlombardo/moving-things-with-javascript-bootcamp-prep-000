52517c1fcc94faf3cf95085bfe846f0996227a9d
sincere-trainer-5337 <eric.m.lombardo@gmail.com>
2020-09-04 16:25:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const app = "I don't do much."
+
+// create an event listener for key down of the id dodger
+document.addEventListener('keydown', function(keys) {
+  if (keys.which === 37) {
+    alert('bingo');
+  }
+});
+// if value equals left run moveLeft func
+// if value equals right run moveRight func
