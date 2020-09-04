1a081a8dbd0adf878a95080b6e5c512e05c05257
sincere-trainer-5337 <eric.m.lombardo@gmail.com>
2020-09-04 16:41:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const app = "I don't do much."
 
 function moveDodgerLeft() {
   // get dodger / remove px / turn into number / assign dodger to left - 1
-  var dodger = $('#dodger');
+  var dodger = document.getElementById('dodger');
+  
   console.log(dodger);
 }
 function moveDodgerRight() {
