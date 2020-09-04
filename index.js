b9babe0fd3daf4e8cf15e521da08b650b8e19f00
sincere-trainer-5337 <eric.m.lombardo@gmail.com>
2020-09-04 16:51:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const app = "I don't do much."
 
 function moveDodgerLeft() {
   // get dodger / remove px / turn into number / assign dodger to left - 1
-  var dodger = document.getElementById('dodger');
   var leftNumbers = dodger.style.left.replace('px', '');
   var left = parseInt(leftNumbers, 10);
   dodger.style.left = `${left - 1}`;
@@ -12,16 +11,15 @@ function moveDodgerRight() {
 }
 // create an event listener for key down of the id dodger
 document.addEventListener('keydown', function(keys) {
+  var dodger = document.getElementById('dodger');
   // if left key is pressed
   if (keys.which === 37) {
     // run function
     moveDodgerLeft();
-    // stop if left ==== 0 (you hit the wall)
   // if right key is pressed
   } if (keys.which === 39) {
     // run function
     moveDodgerRight();
-    // stop if right ==== 0 (you hit the wall)
   }
 });
 
