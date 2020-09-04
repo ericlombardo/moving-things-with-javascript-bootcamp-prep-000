52110c236de96eb65450d2f27d6566ea9de2120d
sincere-trainer-5337 <eric.m.lombardo@gmail.com>
2020-09-04 17:38:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ function moveDodgerRight() {
 
   var leftNumbers = dodger.style.left.replace('px', '');
   var left = parseInt(leftNumbers, 10);
-  dodger.style.left = `${left + 1}px`;
+  if (dodger.style.right > 0) {
+    dodger.style.left = `${left + 1}px`;  
+  }
 }
 // create an event listener for key down of the id dodger
 document.addEventListener('keydown', function(keys) {
