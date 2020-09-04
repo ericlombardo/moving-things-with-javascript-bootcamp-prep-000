653da1788433c3fde9ba956bd098054059077728
sincere-trainer-5337 <eric.m.lombardo@gmail.com>
2020-09-04 16:52:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const app = "I don't do much."
 
 function moveDodgerLeft() {
-  // get dodger / remove px / turn into number / assign dodger to left - 1
+  // remove px / turn into number / assign dodger to left - 1
   var leftNumbers = dodger.style.left.replace('px', '');
   var left = parseInt(leftNumbers, 10);
+  
   dodger.style.left = `${left - 1}`;
 }
 function moveDodgerRight() {
@@ -24,6 +25,5 @@ document.addEventListener('keydown', function(keys) {
 });
 
 
-Working on the movedodger left function
-need to look at an if statement and think through the flow
-when a key is pressed, check if left is 0 else run function
+
+
